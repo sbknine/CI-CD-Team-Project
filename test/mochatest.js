977723f4adbb4aae77794accad6b9dd13543d824
sbknine <thanakrit.n@hotmail.com>
2021-12-16 23:51:04 +0700

    finally we got 1 test

--- test/mochatest.js
+++ test/mochatest.js
@@ -1,2 +1,26 @@
-const {expect} = require("chai")
-const sinon = require("sinon")
+import {expect} from "chai"
+import sinon  from "sinon"
+import User from '../backend/models/userModel.js'
+
+import {authUser} from '../backend/controllers/userController.js'
+
+it("given correct userId should retrieve users full name" , async () => {
+
+    //given
+    const fixture = {
+        _id: "61b9bb954bbb9f2cb82d0300", 
+        email: "admin@example.com", 
+        isAdmin: true, 
+        name: "Admin User"
+    }
+
+    let stub = sinon.stub(User , "findOne").returns(fixture)
+
+    //when
+    let result = await User.findOne('admin@example.com')
+
+    //then
+    expect(result.email).to.eq("admin@example.com")
+    stub.restore()
+
+})
